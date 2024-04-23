86f4517a57355f18a528bac9e44cbac42df5e66d
brenno <brennolyrio@gmail.com>
2024-04-23 09:33:37 -0300

    Meio bagunçado mas testes

--- app.py
+++ app.py
@@ -5,5 +5,13 @@ app = Flask('__name__')
 def home():
     return render_template("index.html")
 
+@app.route('/jedis')
+def jedis():
+    return render_template("jedis.html")
+
+@app.route('/lightsabers')
+def lightsabers():
+    return render_template("lightsabers.html")
+
 if __name__ == '__main__':
     app.run(debug = True)
