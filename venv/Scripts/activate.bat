1ddc6f410bc25e23c56f5e6a7b6a6cc61524aad1
BrennoLyrio <brennolyrio@gmail.com>
2024-04-23 23:06:45 -0300

    Terminado

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Manhã\Desktop\Desafio 3\Trabalho3\venv
+set VIRTUAL_ENV=C:\Users\Brenno\OneDrive\Área de Trabalho\Programação\Bootstrap\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
